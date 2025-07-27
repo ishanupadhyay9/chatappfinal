6ac6964b767a74d64966f5ebdec50fa8000f4e22
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-07-27 12:51:38 +0530

    theme retro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ import FindPage from "./pages/FindPage.jsx";
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
-  const theme  = "caramellatte";
+  const theme  = "retro";
 
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -31,7 +31,7 @@ const LoginPage = () => {
   return (
     <div
       className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
-      data-theme="caramellatte"
+      data-theme="retro"
     >
       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
         {/* LOGIN FORM SECTION */}
