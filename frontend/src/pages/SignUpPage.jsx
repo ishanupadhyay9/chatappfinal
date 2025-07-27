e739c8b6a63fe5324dde38ada0c5018c45c8eb72
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-07-27 12:57:09 +0530

    logo color

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -41,7 +41,7 @@ const SignUpPage = () => {
           {/* LOGO */}
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="size-9 text-primary" />
-            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-amber-700 to-primary tracking-wider">
               QuickChat
             </span>
           </div>
