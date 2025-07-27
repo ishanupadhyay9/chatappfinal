6c4f81d855a082ee98d540cbcdcca3a51ce25c22
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-07-27 21:16:53 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -47,7 +47,7 @@ const HomePage = () => {
   }, [outgoingFriendReqs]);
 
   return (
-     <div className="p-4 sm:p-6 lg:p-8 bg-[url('../bg.png')]">
+     <div className="p-4 sm:p-6 lg:p-8 bg-red">
       <div className="container mx-auto space-y-10 bg">
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
           <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">Your Friends</h2>
